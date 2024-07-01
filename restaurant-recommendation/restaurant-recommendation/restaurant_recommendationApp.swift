dba4adf549f33cff6301e50c650de4ca2f85da67
ethanylhuang <53070830+ethanylhuang@users.noreply.github.com>
2024-07-01 11:37:01 -0700

    Created xcode project

---
+++ restaurant-recommendation/restaurant-recommendation/restaurant_recommendationApp.swift
@@ -0,0 +1,17 @@
+//
+//  restaurant_recommendationApp.swift
+//  restaurant-recommendation
+//
+//  Created by Ethan Huang on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct restaurant_recommendationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ restaurant-recommendation/restaurant-recommendationTests/restaurant_recommendationTests.swift
@@ -0,0 +1,17 @@
+//
+//  restaurant_recommendationTests.swift
+//  restaurant-recommendationTests
+//
+//  Created by Ethan Huang on 7/1/24.
+//
+
+import Testing
+@testable import restaurant_recommendation
+
+struct restaurant_recommendationTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
