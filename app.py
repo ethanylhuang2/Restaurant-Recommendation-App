fe84196fecc1b7368e23589eb67474c293db232e
ethanylhuang <53070830+ethanylhuang@users.noreply.github.com>
2024-07-16 15:37:14 -0700

    Create image links database

---
+++ app.py
@@ -0,0 +1,4 @@
+from flask import Flask, request, jsonify
+from models import Restaurant
+
+app = Flask(__name__)
--- restaurant-recommendation/restaurant-recommendation/ContentView.swift
+++ restaurant-recommendation/restaurant-recommendation/ContentView.swift
@@ -6,17 +6,57 @@
 //
 
 import SwiftUI
+import Combine
+
+let yelpAPIKey = "ZdpeG8xFeTZRSqqiNnbe6tvcV9OI6FKenEdeuEpMLuUhq41DQ9EX9-71yKGfsIvxW9mYIzqZznCG2AuECKuQVOmGUvti485q_e_dS4OxSi3ZCSqwcABXYhdjyr9oZnYx"
+
+struct Restaurant : Identifiable {
+    let id: Int
+    let name: String
+    let imageName: String
+    var rating: Int
+}
+
 
 struct ContentView: View {
+    @State private var restaurant = Restaurant(id: 1, name: "Hello", imageName: "none", rating: 0)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(restaurant.imageName)
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+                .overlay(
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .opacity(0.7)
+                        .edgesIgnoringSafeArea(.all)
+                )
+                
+            VStack {
+                Text(restaurant.name)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding()
+                    
+                Spacer()
+                    .frame(height: 150)
+                
+                HStack {
+                    ForEach(1..<6) { star in
+                        Image(systemName: star <= restaurant.rating ? "star.fill" : "star")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(star <= restaurant.rating ? .yellow : .gray)
+                                .onTapGesture {
+                                    restaurant.rating = star
+                                }
+                        }
+                    }
+                }
+            }
         }
-        .padding()
-    }
 }
 
 #Preview {
